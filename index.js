c9ddea69ad77c667c5a217c688a3f8015dae5bc0
leela khadka <khadkaleela3@gmail.com>
2022-08-09 20:10:25 -0700

    completed tests

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// code your solution here
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+}
+
+function mondayWork(activity = 'go to the office') {
+    return `This Monday, I will ${activity}.`;
+
+}
+
+function wrapAdjective(adjective) {
+    const result = function result(feedback) {
+        return `You are ${adjective}${feedback}${adjective}!`;
+    };
+    return result;
+}
+
+let result = wrapAdjective('*')
+let emphatic = result("a hard worker")
+
+result = wrapAdjective("||")
+emphatic = result("a dedicated programmer")
